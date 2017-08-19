8076b8539261aa5019ca9764804b4373b74125e3
ben ouattara <benny.ouattara@gmail.com>
2017-08-19 15:28:07 -0400

    remove file origin/viewer origin comparison

--- app/assets/javascripts/pdfjs_viewer/viewer.js
+++ app/assets/javascripts/pdfjs_viewer/viewer.js
@@ -7147,17 +7147,19 @@ var HOSTED_VIEWER_ORIGINS = ['null',
 function validateFileURL(file) {
   try {
     var viewerOrigin = new URL(window.location.href).origin || 'null';
+    console.log(viewerOrigin)
     if (HOSTED_VIEWER_ORIGINS.indexOf(viewerOrigin) >= 0) {
       // Hosted or local viewer, allow for any file locations
       return;
     }
     var fileOrigin = new URL(file, window.location.href).origin;
+    console.log(fileOrigin)
     // Removing of the following line will not guarantee that the viewer will
     // start accepting URLs from foreign origin -- CORS headers on the remote
     // server must be properly configured.
-    if (fileOrigin !== viewerOrigin) {
+    /*if (fileOrigin !== viewerOrigin) {
       throw new Error('file origin does not match viewer\'s');
-    }
+    }*/
   } catch (e) {
     var message = e && e.message;
     var loadingErrorMessage = mozL10n.get('loading_error', null,
